5869ce0976c081b829e6034cf78d009ae5e2fc1b
Shail Avasthi <shail.avasthi@yahoo.com>
2020-08-04 13:36:41 -0700

    Created app factory and config

---
+++ config.py
@@ -0,0 +1,19 @@
+from os import environ, path
+from dotenv import load_dotenv
+
+basedir = path.abspath(path.dirname(__file__))
+load_dotenv(path.join(basedir, '.env'))
+
+
+class Config:
+    """Set Flask config variables."""
+
+    FLASK_ENV = 'development'
+    TESTING = True
+    SECRET_KEY = environ.get('SECRET_KEY')
+    STATIC_FOLDER = 'static'
+    TEMPLATES_FOLDER = 'templates'
+
+    # Database
+    #SQLALCHEMY_DATABASE_URI = environ.get('SQLALCHEMY_DATABASE_URI')
+    #SQLALCHEMY_TRACK_MODIFICATIONS = False
